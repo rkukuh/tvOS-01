e6be48081d05daf65f1dfa6d698b93c1cc947700
R. Kukuh <rkukuh@gmail.com>
2021-04-29 09:52:32 +0700

    Rename view

---
+++ tvOS 01/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  tvOS 01
+//
+//  Created by R. Kukuh on 29/04/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Halo")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MainView()
+        }
+    }
+}
--- tvOS 01/tvOS_01App.swift
+++ tvOS 01/tvOS_01App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct tvOS_01App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
