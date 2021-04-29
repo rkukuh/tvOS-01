0f264f73df70067b26a7ab8910c80a31f4e0da82
R. Kukuh <rkukuh@gmail.com>
2021-04-29 14:26:15 +0700

    Create blank explorer detail page

---
+++ tvOS 01/Views/Explorer Views/ExplorerDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ExplorerDetailView.swift
+//  tvOS 01
+//
+//  Created by R. Kukuh on 29/04/21.
+//
+
+import SwiftUI
+
+struct ExplorerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExplorerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExplorerDetailView()
+    }
+}
