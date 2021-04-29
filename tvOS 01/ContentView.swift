c4bbe413fd85d3d5e9701a3ad57be350f9c374d6
R. Kukuh <rkukuh@gmail.com>
2021-04-29 09:19:13 +0700

    Initial Commit

---
+++ tvOS 01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tvOS 01
+//
+//  Created by R. Kukuh on 29/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tvOS 01/tvOS_01App.swift
@@ -0,0 +1,17 @@
+//
+//  tvOS_01App.swift
+//  tvOS 01
+//
+//  Created by R. Kukuh on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct tvOS_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
